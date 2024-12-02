f6d22d3d0542e18087a2636e7642ae122567e231
Mochamad Yoga Sutomo <57779080+s778@users.noreply.github.com>
2024-12-02 22:59:18 +0700

    Update server-stats.sh
    add other metrics

--- Server-Statistic/server-stats.sh
+++ Server-Statistic/server-stats.sh
@@ -23,3 +23,23 @@ ps -eo pid,comm,%cpu --sort=-%cpu
 # List of Processes by Memory Usage
 echo -e "\nProcesses by Memory Usage:"
 ps -eo pid,comm,%mem --sort=-%mem
+
+# Load Average
+echo -e "\nLoad Average (1/5/15 min):"
+uptime | awk -F'load average:' '{print $2}'
+
+# OS Version
+echo -e "\nOS Version:"
+cat /etc/os-release | grep -E "PRETTY_NAME"
+
+# Uptime
+echo -e "\nSystem Uptime:"
+uptime -p
+
+# Logged-In Users
+echo -e "\nLogged-In Users:"
+who
+
+# Failed Login Attempts
+echo -e "\nFailed Login Attempts:"
+grep "Failed password" /var/log/auth.log 2>/dev/null | wc -l || echo "Log file not accessible or not available."
