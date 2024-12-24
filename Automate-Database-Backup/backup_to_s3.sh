47d4e5ee4dd5d4445d5cea3fe46f9035972635cb
Mochamad Yoga Sutomo <57779080+mYsutomo10@users.noreply.github.com>
2024-12-24 22:12:25 +0700

    Create backup_to_s3.sh

---
+++ Automate-Database-Backup/backup_to_s3.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DB_NAME="dbname"
+BACKUP_DIR="/tmp/mongo_backups"
+TIMESTAMP=$(date "Y-%m-%d_%H-%M-%S")
+BACKUP_FILE="${BACKUP_DIR}/${DB_NAME}_${TIMESTAMP}.tar.gz"
+S3_BUCKET_NAME="bucket_name"
+AWS_REGION="ap-southeast-1"
+
+mkdir -p $BACKUP_DIR
+
+mongodump --db $DB_NAME --out $BACKUP_DIR/$DB_NAME-$TIMESTAMP
+
+tar -czvf $BACKUP_FILE -C $BACKUP_DIR $DB_NAME-$TIMESTAMP
+
+aws s3 cp $BACKUP_FILE s3://$S3_BUCKET_NAME/ --region $AWS_REGION
+
+rm -rf $BACKUP_DIR/$DB_NAME-$TIMESTAMP
+rm -rf $BACKUP_FILE
+
+echo "Backup completed"
