e53cda23b03553882148ddaab9a70de70fb8ea97
Mochamad Yoga Sutomo <57779080+mYsutomo10@users.noreply.github.com>
2024-12-24 22:18:29 +0700

    Update restore_from_s3.sh

--- Automate-Database-Backup/restore_from_s3.sh
+++ Automate-Database-Backup/restore_from_s3.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-DB_NAME="myDatabase"
+DB_NAME="dbname"
 RESTORE_DIR="/tmp/mongo_restore"
 S3_BUCKET_NAME="bucket_name"
 AWS_REGION="ap-southeast-1"
+
+
+LATEST_BACKUP=$(aws s3 ls s3://$S3_BUCKET_NAME/ --region $AWS_REGION | sort | tail -n 1 | awk '{print $4}')
+
+mkdir -p $RESTORE_DIR
+aws s3 cp s3://$BUCKET_NAME/$LATEST_BACKUP $RESTORE_DIR --region $AWS_REGION
+
+tar -xzvf $RESTORE_DIR/$LATEST_BACKUP -C $RESTORE_DIR
+mongostore --db $DB_NAME $RESTORE_DIR/$DB_NAME
+
+rm -rf $RESTORE_DIR
+
+echo "Database restored"
